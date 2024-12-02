aa41d2c50d433caffe97019a110e40b2cf7593bd
Winyumi <29512420+Winyumi@users.noreply.github.com>
2024-12-02 13:04:35 -0500

    add pasta

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
-import PizzaController from "@/controllers/pizza.controller.ts";
 import { Router } from "@oak/oak";
+
+import PastaController from "@/controllers/pasta.controller.ts";
+import PizzaController from "@/controllers/pizza.controller.ts";
 
 const router = new Router();
 
+router.use("/pasta", PastaController.routes());
 router.use("/pizza", PizzaController.routes());
-
 router.get("/", (ctx) => {
   ctx.response.body = "Hello Deno!";
 });
